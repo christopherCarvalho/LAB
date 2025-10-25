4b8205496d837c9c67e49be3ffafaefb9f828228
Christopher Cassius <christophercassius27@gmail.com>
2025-10-25 12:52:18 -0300

    Lab exercises

---
+++ Exercises/ex1.1.cpp
@@ -0,0 +1,10 @@
+// expression maths 
+ 
+#include <iostream> 
+using namespace std;
+
+int main(){
+    
+    cout << "(1.2*0.1) + (3.3 * 0.3)/0.09 + 0.001 = " << (1.2 * 0.1) + (3.3 * 0.3)/0.09 + 0.001 << endl;
+    return 0;
+}
