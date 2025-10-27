53f508cc974c4a19e13a5e6a40f02c3615a01562
Christopher Cassius <christophercassius27@gmail.com>
2025-10-27 11:29:09 -0300

    Lab exercises

---
+++ Exercises/ex1.8.cpp
@@ -0,0 +1,24 @@
+// average em kilograms
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    double grocery = 5553.0;
+    double hours, minutes, seconds, kilograms;
+
+    cout << "Enter time: (hours, minutes, seconds) " << endl;
+    cin >> hours, minutes, seconds;
+
+    
+    minutes = hours * 60;       //minutos em horas
+    seconds = minutes * 60;     // segundos em minutos
+
+    kilograms = (grocery/seconds);  //kilograma por segundo
+
+    cout << fixed << setprecision(6) << "Media de kilograma por hora: " << kilograms << "Kg/s"<< endl;
+
+
+    return 0;
+}
